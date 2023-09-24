1dae887fd12042ac81637386f035b41fb9390c0b
54646sc <145828566+54646sc@users.noreply.github.com>
2023-09-24 12:56:22 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int a = 60;
+	
+	if(a<18)
+	    printf("未成年\n");
+	else
+	    printf("成年\n");
+	return 0;
+}
---
+++ 未命名1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	int a = 10;
+	if(a<18)
+	    printf("Î´³ÉÄê\n");
+	return 0;
+} 
