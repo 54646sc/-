c3726091329c43c50f6077bbf85b2ecee4ce79c8
54646sc <145828566+54646sc@users.noreply.github.com>
2023-09-26 23:21:43 +0800

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,22 +1,5 @@
 #include <stdio.h>
 int main(){
-	int w = 0;
-	scanf("%d", &w);
-	switch(w)
-	{
-	case 1:
-	case 2:
-	case 3:
-	case 4:	
-	case 5:
-	    printf("工作日\n");
-		break;
-	case 6:
-	case 7:
-	    printf("休息日\n");
-		break; 
-	default:
-	    printf ("输入错误\n");
-	}
+	printf("%d", 4);
 	return 0;
-} 
+}
