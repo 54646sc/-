4e98ca908a65a0106c702b8d6c53d9ab58030fc1
54646sc <145828566+54646sc@users.noreply.github.com>
2023-10-18 23:24:35 +0800

    Add files via upload

--- 13.cpp
+++ 13.cpp
@@ -1,6 +1,11 @@
-//在屏幕上显示两条语句。
 #include <stdio.h>
 int main(){
-	printf("programming is fun.\nand programming is c is even more fun.");
+	int a,b;
+	scanf("%d%d",&a,&b);
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	printf("%d %d",a,b);
 	return 0;
 } 
