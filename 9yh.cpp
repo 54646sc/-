64e71dcdc3303492b95b990f0de32da2177caa32
54646sc <145828566+54646sc@users.noreply.github.com>
2023-11-18 18:59:53 +0800

    Add files via upload

---
+++ 9yh.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+enum um
+{
+	a,
+	b,
+	c
+};
+int main(){
+	int i;
+	scanf("%d",&i);
+	if(i==b)
+	{
+		printf("1");
+	}
+	return 0;
+}
---
+++ u.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int t=2;
+	while(--t>0)
+	{
+		printf("1 ");
+	}
+	return 0;
+}
